a95d67abd4f3ccd3db9a716b56c4b1dadbc55fac
nekoshell <nekoshell.jyo@gmail.com>
2015-05-11 17:47:23 +0800

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by 徐文亮 on 15/5/11.
+//  Copyright (c) 2015年 徐文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by 徐文亮 on 15/5/11.
+//  Copyright (c) 2015年 徐文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
